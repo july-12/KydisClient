607dc897a30004b7f90fd91b9e3601e52510b68b
Faith <seventanyb@gmail.com>
2018-10-31 13:54:48 +0800

    post form

--- src/views/posts/_form.js
+++ src/views/posts/_form.js
@@ -1,11 +1,81 @@
 import React, { Component } from 'react'
 
-import { Pane } from 'evergreen-ui';
+import { Pane, Button, Label, TextInput, Textarea, Select } from 'evergreen-ui';
 
 export default class Form extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      title: '',
+      categoryId: '',
+      content: ''
+    }
+  }
+  handleSubmit = (e) => {
+    e.preventDefault()
+    let { title, content, categoryId } = this.state
+    let data = { title, content, categoryId }
+    this.props.onSubmit(data)
+  }
   render() {
     return (
-      <Pane height="500px"> this shoule be form page. </Pane>
+      <Pane marginTop={80}>
+        <Pane>
+          <Label
+             width={100}
+             htmlFor="title"
+             marginRight={20}
+             textAlign="right"
+             display="inline-block"
+          >
+            Title:
+          </Label>
+          <TextInput id="title" width={300} name="title" placeholder="Please input title..." />
+        </Pane>
+        <Pane marginTop={30}>
+          <Label
+             width={100}
+             htmlFor="category"
+             marginRight={20}
+             textAlign="right"
+             display="inline-block"
+          >
+            Category:
+          </Label>
+          <Select id="category" width={300} onChange={event => console.log(event.target.value)}>
+              <option value="foo" checked>Foo</option>
+              <option value="bar">Bar</option>
+          </Select>
+        </Pane>
+        <Pane display="flex" marginTop={30}>
+          <Label
+             width={100}
+             htmlFor="content"
+             marginRight={20}
+             textAlign="right"
+             display="inline-block"
+          >
+            Content:
+          </Label>
+          <Textarea
+            height={200}
+            width={600}
+            id="content"
+            placeholder="Please input content..."
+          />
+        </Pane>
+        <Pane marginTop={30} marginLeft={110} >
+          <Button 
+            width={100}
+            display="inline-block" 
+            textAlign="center" 
+            appearance="primary"
+            onClick={this.handleSubmit}
+          >
+            Submit
+          </Button>
+        </Pane>
+      </Pane>
     )
   }
 }
